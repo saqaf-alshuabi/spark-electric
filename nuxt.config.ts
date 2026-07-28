18651078a1f90c9f9bcbdce58798236c9aa262da
Saqaf <saqafaliabdo716@gmail.com>
2026-07-28 03:43:44 +0300

    Remove '@formkit/auto-animate' module from nuxt.config.ts and package.json, and update pnpm-lock.yaml accordingly to reflect the changes in dependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui',
-    '@nuxt/image',
-    '@nuxt/fonts',
-    '@formkit/auto-animate'
-  ]
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', '@nuxt/fonts'],
 })
