cae2dd12c711c5865fc36d41a38aadb287345c77
Saqaf <saqafaliabdo716@gmail.com>
2026-07-31 00:06:59 +0300

    feat: add @vueuse/nuxt dependency and update configuration files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', '@nuxt/fonts'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', '@nuxt/fonts', '@vueuse/nuxt'],
   devtools: { enabled: true },
   app: {
     head: {
