0745fbfea544f666f27c14221bad30a4d0367b74
Saqaf <saqafaliabdo716@gmail.com>
2026-08-04 01:38:14 +0300

    Update nuxt.config.ts to include new modules '@nuxt/hints' and '@nuxtjs/seo'; modify package.json to add corresponding dependencies and downgrade TypeScript version to 5.9.0; update pnpm-lock.yaml to reflect new dependencies and their versions; remove robots.txt file; adjust main.css to clean up unused font-family declaration.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', '@nuxt/fonts', '@vueuse/nuxt'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', '@nuxt/fonts', '@vueuse/nuxt', '@nuxt/hints', '@nuxtjs/seo'],
   devtools: { enabled: true },
   app: {
     head: {
