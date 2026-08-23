da11420877fbcc51cf1ddb3987c50e414c056a6c
Saqaf <saqaf.alshuabi@gmail.com>
2026-08-23 05:25:51 +0300

    Refactor scroll behavior logic in router.options.ts for improved readability
    - Simplified the scrollBehavior function by condensing conditional statements into single-line returns.
    - Enhanced code clarity while maintaining existing functionality.

--- app/router.options.ts
+++ app/router.options.ts
@@ -2,13 +2,8 @@ import type { RouterConfig } from '@nuxt/schema'
 
 export default {
   scrollBehavior(to, _from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition
-    }
-
-    if (to.hash) {
-      return false
-    }
+    if (savedPosition) return savedPosition
+    if (to.hash) return false
 
     return false
   },
